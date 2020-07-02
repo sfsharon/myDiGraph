4d42fd04d799501489d1366fdc069c70a47818b8
Sharon Fraiman <sharon.fraiman@gm.com>
2020-07-03 00:10:28 +0300

    Update readCSV() to handle correctly CSV file

--- myDiGraph/main.c
+++ myDiGraph/main.c
@@ -1,5 +1,6 @@
 #define _CRT_SECURE_NO_DEPRECATE
 #include <stdio.h>
+#include <string.h>
 
 #define MAX_NUM_NODES 10
 
@@ -24,36 +25,20 @@ void readCSV() {
     char buf[1024];
     int row_count = 0;
     int field_count = 0;
+
     while (fgets(buf, 1024, fp)) {
+        char *pVal = NULL;
         field_count = 0;
-        row_count++;
+        pVal = strtok(buf, ",");
 
-        if (row_count == 1) {
-            continue;
-        }
-
-        char *field = strtok(buf, ",");
-        while (field) {
-            if (field_count == 0) {
-                printf("First Name:\t");
-            }
-            if (field_count == 1) {
-                printf("Last Name:\t");
-            }
-            if (field_count == 2) {
-                printf("Phone Number:\t");
-            }
-            if (field_count == 3) {
-                printf("Job Title:\t");
-            }
-
-            printf("%s\n", field);
-            field = strtok(NULL, ",");
-
+        while (pVal != NULL) {
+            printf("[Row_cnt/Field_cnt] [%d/%d] %s\n", row_count, field_count, pVal);
+            // Move to next value
+            pVal = strtok(NULL, ",");
             field_count++;
-        }
-        printf("\n");
-    }
+        } // while(words in line are not empty)
+        row_count++;
+    } // while(lines in file are not empty)
 
     fclose(fp);
 }
