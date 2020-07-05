f9a32569c4b86c15e9c006c03bd293affd1a6eed
Sharon Fraiman <sharon.fraiman@gm.com>
2020-07-05 06:52:22 +0300

    Add stack module

---
+++ myDiGraph/myStack.c
@@ -0,0 +1,16 @@
+/*
+* *****************************************************
+*              myStack module.
+* *****************************************************
+*
+* Program functionality :
+* -----------------------
+* Implement an integer stack
+*
+* Based on https://www.techiedelight.com/stack-implementation/
+*
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
